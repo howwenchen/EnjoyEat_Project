c0e6ad8cf260a65bb2e1d314ec29d93c85b1fa7b
John <mouiu1177@gmail.com>
2023-05-15 15:56:58 +0800

    刪除OrderForHere測試

--- 專案OrderForHere/專案OrderForHere/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace 專案OrderForHere.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
