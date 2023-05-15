620ebd54ad63a866cc965fb193c9d69aeaed6752
John <mouiu1177@gmail.com>
2023-05-15 15:34:53 +0800

    加入用戶端程式庫 vue, axios

---
+++ EnjoyEat/EnjoyEat/Areas/backend/Controllers/EmployeeManagement.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnjoyEat.Areas.backend.Controllers
+{
+    public class EmployeeManagement : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
