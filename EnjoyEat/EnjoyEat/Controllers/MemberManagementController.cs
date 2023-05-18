36e5f1b057eca5dba7003c491c2883b782265974
John <mouiu1177@gmail.com>
2023-05-18 19:10:48 +0800

    Revert "OrderForHere"
    This reverts commit 139f0cebc75f17750b8f6e443e18efebb354a73a.

--- EnjoyEat/EnjoyEat/Controllers/MemberManagementController.cs
+++ EnjoyEat/EnjoyEat/Controllers/MemberManagementController.cs
@@ -6,31 +6,6 @@ namespace EnjoyEat.Controllers
     {
         public IActionResult Index()
         {
-<<<<<<< Updated upstream
-=======
-            using (var _context = new db_a989fe_thm101team6Context())
-            {
-                var memberData = _context.Members;
-                var orderData= _context.Orders;
-                var orderDetailData = _context.OrderDetails;
-                var viewModel = memberData.Select(m => new MemberViewModel
-                {
-                    MemberId = m.MemberId,
-                    FirstName = m.FirstName,
-                    LastName = m.LastName,
-                    Gender = m.Gender,
-                    Birthday = m.Birthday,
-                    Address = m.Address,
-                    Phone = m.Phone,
-                    Email = m.Email,
-                    LevelName = m.LevelName,
-                });
-                return View(viewModel);
-            }
-        }
-        public IActionResult EditPassword()
-        {
->>>>>>> Stashed changes
             return View();
         }
     }
