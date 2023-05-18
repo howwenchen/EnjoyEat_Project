a41fa0f9d6fdde5eeaaab4096dbd6fbd46cb1aea
Howwen <wayne29408295@gmail.com>
2023-05-18 18:44:32 +0800

    managementCOntroller更新

--- EnjoyEat/EnjoyEat/Controllers/MemberManagementController.cs
+++ EnjoyEat/EnjoyEat/Controllers/MemberManagementController.cs
@@ -8,29 +8,15 @@ namespace EnjoyEat.Controllers
 {
     public class MemberManagementController : Controller
     {
-        
-        public IActionResult Index()
+        private readonly db_a989fe_thm101team6Context _db;
+        public MemberManagementController(db_a989fe_thm101team6Context projectContext)
         {
-            using (var _context = new db_a989fe_thm101team6Context())
-            {
-                var memberData = _context.Members;
-                var viewModel = memberData.Select(m => new MemberViewModel
-                {
-                    MemberId = m.MemberId,
-                    FirstName = m.FirstName,
-                    LastName = m.LastName,
-                    Gender = m.Gender,
-                    Birthday = m.Birthday,
-                    Address = m.Address,
-                    Phone = m.Phone,
-                    Email = m.Email,
-                    LevelName = m.LevelName,
-                    Password = m.Password,
-                    MemberPoints = m.MemberPoints,
-                    Orders = m.Orders,
-                });
-                return View(viewModel);
-            }
+            this._db = projectContext;
+        }
+            public IActionResult Index()
+        {
+                return View();
+            
         }
         public IActionResult EditPassword()
         {
