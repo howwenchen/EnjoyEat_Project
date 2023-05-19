c5e3ab0e8a7c6933cd2ba9ca06e5b3240efb1993
Sunny <vans706706@gmail.com>
2023-05-19 11:11:46 +0800

    加入Reservations css js

--- EnjoyEat/EnjoyEat/Controllers/ReservationsController.cs
+++ EnjoyEat/EnjoyEat/Controllers/ReservationsController.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EnjoyEat.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EnjoyEat.Controllers
 {
     public class ReservationsController : Controller
     {
+        private readonly db_a989fe_thm101team6Context _db;
+        public ReservationsController(db_a989fe_thm101team6Context projectContext)
+        {
+            this._db = projectContext;
+        }
         public IActionResult Index()
         {
             return View();
