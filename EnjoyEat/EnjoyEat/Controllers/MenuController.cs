b9b8de151b4462563821806fdec58a258d8ed1c8
Kiki <a29480096@gmail.com>
2023-05-19 14:32:41 +0800

    加入MenuViewModel、修改MenuController

--- EnjoyEat/EnjoyEat/Controllers/MenuController.cs
+++ EnjoyEat/EnjoyEat/Controllers/MenuController.cs
@@ -1,12 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EnjoyEat.Models;
+using EnjoyEat.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace EnjoyEat.Controllers
 {
     public class MenuController : Controller
     {
-        public IActionResult Index()
+        private readonly db_a989fe_thm101team6Context _context;
+
+        public MenuController(db_a989fe_thm101team6Context context)
         {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {  
             return View();
+
+            //var temp = _cnntext.Product.Select(opt => new MenuViewModel()
+            //{
+            //    ProductId = _context.Products.First().ProductId,
+            //    ProductName = _context.Products.First().ProductName,
+            //    UnitPrice = _context.Products.First().UnitPrice,
+            //    CategoryId = _context.Category.First().CategoryId,
+            //    CategoryMaame = _context.Category.Category
+            //});
+            //return View(temp);
         }
     }
 }
+
+        //[HttpGet]
+        //public ActionResult Index()
+        //{
+        //    var emps = new List<MenuViewModel> {
+        //        new MenuViewModel {ProductName="rico", UnitPrice=35},
+        //        new MenuViewModel {ProductName="sherry",UnitPrice=30},
+        //        new MenuViewModel {ProductName="fifi",UnitPrice=4 },
+        //        new MenuViewModel {ProductName="rico2",UnitPrice=34 },
+        //        new MenuViewModel {ProductName="sherry2",UnitPrice=29 },
+        //        new MenuViewModel {ProductName="fifi2",UnitPrice=3 },
+        //        new MenuViewModel {ProductName="rico3",UnitPrice=33 },
+        //        new MenuViewModel {ProductName="sherry3",UnitPrice=28 },
+        //        new MenuViewModel {ProductName="fifi3",UnitPrice=2},
+        //        new MenuViewModel {ProductName="Vue",UnitPrice=1}
+        //    };
+        //    var serverModel = JsonConvert.SerializeObject(emps);
+        //    return View(new { serverModel });
---
+++ EnjoyEat/EnjoyEat/Models/ViewModel/MenuViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EnjoyEat.Models.ViewModel
+{
+    public class MenuViewModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public int UnitPrice { get; set; }
+        public byte CategoryId { get; set; }
+
+        public virtual Category Category { get; set; } = null!;
+    }
+}
