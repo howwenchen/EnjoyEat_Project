c9e9854f36b6785235a097f010ff8792965fbe90
John <mouiu1177@gmail.com>
2023-05-19 23:25:10 +0800

    add StartOrder、ShoppingCart

---
+++ EnjoyEat/EnjoyEat/Areas/OrderForHere/Controllers/ShoppingCartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnjoyEat.Areas.OrderForHere.Controllers
+{
+    [Area("OrderForHere")]
+    public class ShoppingCartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
