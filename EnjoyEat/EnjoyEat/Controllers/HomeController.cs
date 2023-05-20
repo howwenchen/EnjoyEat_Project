71b42056e290c56cf4d44241dcbc97c6bf8f1f7b
Howwen <wayne29408295@gmail.com>
2023-05-20 11:41:25 +0800

    conflict

--- EnjoyEat/EnjoyEat/Controllers/HomeController.cs
+++ EnjoyEat/EnjoyEat/Controllers/HomeController.cs
@@ -1,70 +1,28 @@
 ﻿using EnjoyEat.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using System.Linq;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
 
 namespace EnjoyEat.Controllers
 {
-    public class HomeController : Controller
-    {
-        public readonly db_a989fe_thm101team6Context _context;
-        public readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger, db_a989fe_thm101team6Context context)
-        {
-            _logger = logger;
-            _context = context;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        [Route("Home/GetNews/{page?}")]
-        public IActionResult GetNews(int page = 1)
-        {
-            int pageSize = 8;
-            int skipCount = (page - 1) * pageSize;
-
-            var totalNewsCount = _context.News.Count();
-            var totalPageCount = (int)Math.Ceiling(totalNewsCount / (double)pageSize);
-
-            if (totalPageCount == 0)
-            {
-                return BadRequest("No news available.");
-            }
-
-            if (page > totalPageCount)
-            {
-                return BadRequest("The requested page number exceeds the total page count.");
-            }
-
-            var news = _context.News.Skip(skipCount).Take(pageSize).ToList();
-            return Json(news);
-        }
-
-
-
-        [HttpGet]
-        [Route("Home/GetPageCount")]
-        public IActionResult GetPageCount()
-        {
-            int pageSize = 8;
-            var pageCount = (int)Math.Ceiling(_context.News.Count() / (double)pageSize);
-
-            // 回傳頁數
-            return Json(pageCount);
-        }
         public IActionResult News()
         {
             return View();
