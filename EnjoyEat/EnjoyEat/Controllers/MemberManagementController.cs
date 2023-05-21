4be94ca5646dde167ed7dd872f963bc7da1cdcf8
leon183628 <64385669+leon183628@users.noreply.github.com>
2023-05-21 15:33:43 +0800

    修正衝突

--- EnjoyEat/EnjoyEat/Controllers/MemberManagementController.cs
+++ EnjoyEat/EnjoyEat/Controllers/MemberManagementController.cs
@@ -12,16 +12,27 @@ namespace EnjoyEat.Controllers
         public MemberManagementController(db_a989fe_thm101team6Context db)
         {
 
-        {
-            return View();
-
+            using (var _context = new db_a989fe_thm101team6Context())
+            {
+                var memberData = _context.Members;
+                var orderData = _context.Orders;
+                var orderDetailData = _context.OrderDetails;
+                var viewModel = memberData.Select(m => new MemberViewModel
+                {
+                    MemberId = m.MemberId,
+                    FirstName = m.FirstName,
+                    LastName = m.LastName,
+                    Gender = m.Gender,
+                    Birthday = m.Birthday,
+                    Address = m.Address,
+                    Phone = m.Phone,
+                    Email = m.Email,
+                    LevelName = m.LevelName,
+                    //Password = m.Password,
+                });
+                return View(viewModel);
+            }
         }
-
-        public IActionResult EditPassword()
-        {
-            return View();
-        }
-
-
+        public IActionResult Index()
     }
 }
