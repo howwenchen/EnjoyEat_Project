ee8505c054168ae8cbc8cd0a95aaa4460ffa991f
Howwen <wayne29408295@gmail.com>
2023-05-24 16:51:06 +0800

    訂位資料庫修改

--- EnjoyEat/EnjoyEat/Models/Reservation.cs
+++ EnjoyEat/EnjoyEat/Models/Reservation.cs
@@ -16,7 +16,7 @@ namespace EnjoyEat.Models
         public DateTime? ConfirmationDate { get; set; }
         public string? NumberofAdultGuest { get; set; }
         public string? NumberofKidGuest { get; set; }
-        public DateTime? ReservationTime { get; set; }
+        public string? ReservationTime { get; set; }
 
         public virtual ICollection<ReservationInformation> ReservationInformations { get; set; }
     }
