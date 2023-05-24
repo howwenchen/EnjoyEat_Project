73f38734b5548a588d6cb6f1fba5a7a7332f9bd7
Sunny <vans706706@gmail.com>
2023-05-24 13:52:49 +0800

    新增聯絡我們VUE

---
+++ EnjoyEat/EnjoyEat/Controllers/API/ReservationInformationsAPIController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EnjoyEat.Models;
+using EnjoyEat.Models.ViewModel;
+
+namespace EnjoyEat.Controllers.API
+{
+    [Route("api/reservation/booking/[action]")]
+    [ApiController]
+    public class ReservationInformationsAPIController : ControllerBase
+    {
+        private readonly db_a989fe_thm101team6Context db;
+
+        public ReservationInformationsAPIController(db_a989fe_thm101team6Context db)
+        {
+            this.db = db;
+        }
+
+        // POST: api/ReservationInformationsAPI
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<string> PostReservationInformation(ReservationInformationViewModel reservationinformationViewModel)
+        {
+            ReservationInformation reservationInformation = new ReservationInformation
+            {
+                PhoneNumber = reservationinformationViewModel.PhoneNumber,
+                ReservationName = reservationinformationViewModel.ReservationName,
+                EMail = reservationinformationViewModel.EMail,
+                Note = reservationinformationViewModel.Note,
+            };
+            db.ReservationInformations.Add(reservationInformation);
+            await db.SaveChangesAsync();
+            return "訂位成功!";
+        }
+
+        private bool ReservationInformationExists(string id)
+        {
+            return (db.ReservationInformations?.Any(e => e.PhoneNumber == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ EnjoyEat/EnjoyEat/Controllers/API/ReservationsAPIController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EnjoyEat.Models;
+using EnjoyEat.Models.ViewModel;
+
+namespace EnjoyEat.Controllers.API
+{
+    [Route("api/reservation/[action]")]
+    [ApiController]
+    public class ReservationsAPIController : ControllerBase
+    {
+        private readonly db_a989fe_thm101team6Context db;
+
+        public ReservationsAPIController(db_a989fe_thm101team6Context db)
+        {
+            this.db = db;
+        }
+
+        // POST: api/ReservationsAPI
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<string> PostReservation(ReservationViewModel reservationViewModel)
+        {
+            Reservation reservation = new Reservation
+            {
+                PhoneNumber = reservationViewModel.PhoneNumber,
+                ReservationDate = reservationViewModel.ReservationDate,
+                NumberofAdultGuest = reservationViewModel.NumberofAdultGuest,
+                NumberofKidGuest = reservationViewModel.NumberofKidGuest,
+                ReservationTime = reservationViewModel.ReservationTime,
+
+            };
+            db.Reservations.Add(reservation);
+            await db.SaveChangesAsync();
+            return "轉下一頁";
+        }
+
+        private bool ReservationExists(string id)
+        {
+            return (db.Reservations?.Any(e => e.PhoneNumber == id)).GetValueOrDefault();
+        }
+    }
+}
