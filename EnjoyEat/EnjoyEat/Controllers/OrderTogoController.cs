3f67fe61490f9850b7786a1460f77e30ac364d29
Kiki <a29480096@gmail.com>
2023-05-24 11:50:10 +0800

    修改OrderTogo

--- EnjoyEat/EnjoyEat/Controllers/OrderTogoController.cs
+++ EnjoyEat/EnjoyEat/Controllers/OrderTogoController.cs
@@ -1,16 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EnjoyEat.Models.ViewModel;
+using EnjoyEat.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EnjoyEat.Controllers
 {
     public class OrderTogoController : Controller
     {
+        private readonly db_a989fe_thm101team6Context _context;
+
+        public OrderTogoController(db_a989fe_thm101team6Context context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
         public IActionResult Order()
         {
-            return View();
+            var dbContext = _context.Products.Include(t => t.SubCategory);
+            var temp = dbContext.Select(pro => new MenuViewModel
+            {
+                ProductId = pro.ProductId,
+                MealImg = pro.MealImg,
+                ProductName = pro.ProductName,
+                UnitPrice = pro.UnitPrice,
+                Description = pro.Description,
+                CategoryName = pro.SubCategory.Category.CategoryName
+            });
+
+            return View(temp);
         }
         public IActionResult Contact()
         {
@@ -18,3 +40,4 @@ namespace EnjoyEat.Controllers
         }
     }
 }
+
