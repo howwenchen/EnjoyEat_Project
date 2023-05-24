6fe6354403d6583c29a3d519fcac394759cd6b8a
Sunny <vans706706@gmail.com>
2023-05-24 14:05:46 +0800

    修改viewmodel

--- EnjoyEat/EnjoyEat/Models/ViewModel/ReservationInformationViewModel.cs
+++ EnjoyEat/EnjoyEat/Models/ViewModel/ReservationInformationViewModel.cs
@@ -5,7 +5,7 @@
         public string PhoneNumber { get; set; } = null!;
         public string? ReservationName { get; set; }
         public string? EMail { get; set; }
+        public string? Note { get; set; }
 
-        public virtual Reservation PhoneNumberNavigation { get; set; } = null!;
     }
 }
--- EnjoyEat/EnjoyEat/Models/ViewModel/ReservationViewModel.cs
+++ EnjoyEat/EnjoyEat/Models/ViewModel/ReservationViewModel.cs
@@ -4,11 +4,9 @@
     {
         public string PhoneNumber { get; set; } = null!;
         public DateTime ReservationDate { get; set; }
-        public DateTime ConfirmationDate { get; set; }
         public string NumberofAdultGuest { get; set; } = null!;
         public string? NumberofKidGuest { get; set; }
         public DateTime ReservationTime { get; set; }
 
-        public virtual ReservationInformation? ReservationInformation { get; set; }
     }
 }
