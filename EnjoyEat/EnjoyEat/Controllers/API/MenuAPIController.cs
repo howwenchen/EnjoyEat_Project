7c06e6995b884eaa1cf665f378198e19e803829a
Kiki <a29480096@gmail.com>
2023-05-25 16:00:04 +0800

    修改menu_index、menu_style、menuAPIController、menuController

--- EnjoyEat/EnjoyEat/Controllers/API/MenuAPIController.cs
+++ EnjoyEat/EnjoyEat/Controllers/API/MenuAPIController.cs
@@ -1,11 +1,35 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EnjoyEat.Models;
+using EnjoyEat.Models.ViewModel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EnjoyEat.Controllers.API
 {
-    [Route("api/[controller]")]
+    [Route("api/menu/[action]")]
     [ApiController]
     public class MenuAPIController : ControllerBase
     {
+        private readonly db_a989fe_thm101team6Context _context;
+        public MenuAPIController(db_a989fe_thm101team6Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetMenu()
+        {
+            var dbContext = _context.Products.Include(t => t.SubCategory);
+            var temp = dbContext.Select(pro => new MenuViewModel
+            {
+                ProductId = pro.ProductId,
+                MealImg = pro.MealImg,
+                ProductName = pro.ProductName,
+                UnitPrice = pro.UnitPrice,
+                Description = pro.Description,
+                CategoryName = pro.SubCategory.Category.CategoryName
+            });
+            return Ok(temp);
+        }
     }
 }
