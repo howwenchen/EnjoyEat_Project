6553d2062719587d44004c392c46ce2afbf0bdbb
Kiki <a29480096@gmail.com>
2023-05-30 15:04:19 +0800

    修改ordertogo_order頁面、控制器，增加ordertogo_checkout頁面

--- EnjoyEat/EnjoyEat/Controllers/OrderTogoController.cs
+++ EnjoyEat/EnjoyEat/Controllers/OrderTogoController.cs
@@ -7,32 +7,40 @@ namespace EnjoyEat.Controllers
 {
     public class OrderTogoController : Controller
     {
-        private readonly db_a989fe_thm101team6Context _context;
+        //private readonly db_a989fe_thm101team6Context _context;
 
-        public OrderTogoController(db_a989fe_thm101team6Context context)
-        {
-            _context = context;
-        }
+        //public OrderTogoController(db_a989fe_thm101team6Context context)
+        //{
+        //    _context = context;
+        //}
         public IActionResult Index()
         {
             return View();
         }
 
-        [HttpGet]
+        //[HttpGet]
         public IActionResult Order()
         {
-            var dbContext = _context.Products.Include(t => t.SubCategory);
-            var temp = dbContext.Select(pro => new MenuViewModel
-            {
-                ProductId = pro.ProductId,
-                MealImg = pro.MealImg,
-                ProductName = pro.ProductName,
-                UnitPrice = pro.UnitPrice,
-                Description = pro.Description,
-                CategoryName = pro.SubCategory.Category.CategoryName
-            });
+            //var dbContext = _context.Products.Include(t => t.SubCategory);
+            //var temp = dbContext.Select(pro => new MenuViewModel
+            //{
+            //    ProductId = pro.ProductId,
+            //    MealImg = pro.MealImg,
+            //    ProductName = pro.ProductName,
+            //    UnitPrice = pro.UnitPrice,
+            //    Description = pro.Description,
+            //    CategoryName = pro.SubCategory.Category.CategoryName
+            //});
 
-            return View(temp);
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        public IActionResult Checkout()
+        {
+            return View();
         }
         public IActionResult Contact()
         {
