6496d2d570903b1ade0f6af93e9554acb0e1cc4b
Sunny <vans706706@gmail.com>
2023-06-01 19:39:38 +0800

    新增後台訂位管理

---
+++ EnjoyEat/EnjoyEat/Models/ViewModel/ReservationManagemanetViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EnjoyEat.Models.ViewModel
+{
+    public class ReservationManagemanetViewModel
+    {
+        public int ReserveId { get; set; }
+        public DateTime? ReservationDate { get; set; }
+        public string? ReservationTime { get; set; }
+        public string? NumberofAdultGuest { get; set; }
+        public string? NumberofKidGuest { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? ReservationName { get; set; }
+        public string? Email { get; set; }
+        public string? Note { get; set; }
+    }
+}
