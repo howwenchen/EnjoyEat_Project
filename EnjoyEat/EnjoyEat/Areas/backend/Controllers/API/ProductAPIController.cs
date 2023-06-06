ef6ef046e28719ee96c5dc5564f423126e922478
Kiki <a29480096@gmail.com>
2023-06-06 14:40:36 +0800

    更新後台餐點管理

---
+++ EnjoyEat/EnjoyEat/Areas/backend/Controllers/API/ProductAPIController.cs
@@ -0,0 +1,113 @@
+﻿using EnjoyEat.Areas.OrderForHere.Models;
+using EnjoyEat.Models;
+using EnjoyEat.Models.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnjoyEat.Areas.backend.Controllers.API
+{
+    [Route("api/ProductAPI/[action]")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
+    {
+        private readonly db_a989fe_thm101team6Context _context;
+        public ProductAPIController(db_a989fe_thm101team6Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MenuViewModel>>> GetProduct()
+        {
+            try
+            {
+                var dbContext = _context.Products.Include(t => t.SubCategory);
+                var product = await dbContext.AsNoTracking().Select(pro => new MenuViewModel
+                {
+                    ProductId = pro.ProductId,
+                    MealImg = pro.MealImg,
+                    ProductName = pro.ProductName,
+                    Costs = pro.Costs,
+                    UnitPrice = pro.UnitPrice,
+                    Stock = pro.Stock,
+                    Description = pro.Description,
+                    Recipe = pro.Recipe,
+                    CategoryName = pro.SubCategory.Category.CategoryName,
+                    SubCategoriesName = pro.SubCategory.SubCategoriesName,
+                }).ToListAsync();
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return StatusCode(500, "Internal server error");
+            }
+        }
+        public async Task<ActionResult<IEnumerable<CategoryViewModel>>> GetCategory()
+        {
+            try
+            {
+                var category = await _context.Categories.AsNoTracking().Select(cat => new CategoryViewModel
+                {
+                    CategoryId = cat.CategoryId,
+                    CategoryName = cat.CategoryName,
+                }).ToListAsync();
+                return Ok(category);
+            }
+            catch (Exception ex)
+            {
+                // 確認錯誤訊息
+                Console.WriteLine(ex.ToString());
+                return StatusCode(500, "Internal server error");
+            }
+        }
+        public async Task<ActionResult<IEnumerable<SubCategoryViewModel>>> GetSubCategory()
+        {
+            try
+            {
+                var subCategory = await _context.SubCategories.AsNoTracking().Select(cat => new SubCategoryViewModel
+                {
+                    CategoryId = cat.CategoryId,
+                    SubCategoryId = cat.SubCategoryId,
+                    SubCategoriesName = cat.SubCategoriesName,
+                }).ToListAsync();
+                return Ok(subCategory);
+            }
+            catch (Exception ex)
+            {
+                // 確認錯誤訊息
+                Console.WriteLine(ex.ToString());
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpPost]
+        public async Task<string> CreateProduct([FromBody] MenuViewModel meal)
+        {
+            try
+            {
+                Product pro = new Product
+                {
+                    ProductId = meal.ProductId,
+                    MealImg = meal.MealImg,
+                    ProductName = meal.ProductName,
+                    Costs = meal.Costs,
+                    UnitPrice = meal.UnitPrice,
+                    Stock = meal.Stock,
+                    SubCategoryId = meal.SubCategoryId,
+                    Description = meal.Description,
+                    Recipe = meal.Recipe,
+                };
+                _context.Products.Add(pro);
+                await _context.SaveChangesAsync();
+                return "新增成功";
+            }
+            catch (Exception)
+            {
+                return "新增失敗";
+            }
+        }
+    }
+}
--- EnjoyEat/EnjoyEat/Models/DTO/ProductsDTO.cs
+++ EnjoyEat/EnjoyEat/Models/DTO/ProductsDTO.cs
@@ -1,32 +1,30 @@
-﻿namespace EnjoyEat.Models.ViewModel
+﻿namespace EnjoyEat.Models.DTO
 {
-    public class MenuViewModel
+    public class ProductsDTO
     {
-        public MenuViewModel() {
-            MealImg = "/img/food/noPic.jpg";
-        }
         public int ProductId { get; set; }
-        public string MealImg { get; set; }
+        public string? MealImg { get; set; } = null!;
         public string ProductName { get; set; } = null!;
         public short UnitPrice { get; set; }
         public byte Costs { get; set; }
         public short? Stock { get; set; }
         public string? Description { get; set; }
         public string? Recipe { get; set; }
-        public string? CategoryName { get; set; }
-        public string? SubCategoriesName { get; set; }
         public byte SubCategoryId { get; set; }
-        //public virtual ICollection<SubCategory> SubCategory { get; set; } = null!;
+        public virtual SubCategory SubCategory { get; set; } = null!;
+        public string CategoryName { get; internal set; }
+        public string SubCategoriesName { get; internal set; }
     }
-    public class SubCategoryViewModel
+    public class SubCategoryDTO
     {
         public byte CategoryId { get; set; }
         public byte SubCategoryId { get; set; }
         public string SubCategoriesName { get; set; } = null!;
     }
-    public partial class CategoryViewModel
+    public partial class CategoryDTO
     {
         public byte CategoryId { get; set; }
         public string CategoryName { get; set; } = null!;
     }
 }
+
